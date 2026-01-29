0f200b8e06dd7e634eb644a5602f3e6db892fbfa
ajuvi <102821845+ajuvi@users.noreply.github.com>
2026-01-29 09:50:17 +0100

    CompteBancari

--- OrientacioObjectes/CompteBancari/CompteBancari/CompteBancari.cs
+++ OrientacioObjectes/CompteBancari/CompteBancari/CompteBancari.cs
@@ -2,50 +2,98 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DemoErrorsOO
+namespace DemoCompteBancari
 {
-    internal class CompteBancari
+    public class CompteBancari
     {
         // CAMPS
-        public string titular;
+        private string iban;
+        private string titular;
         private double saldo;
         private bool bloquejat;
         private static double tipusInteresAnual = 0.03;
 
         // CONSTRUCTORS
-        public CompteBancari(string titular, double saldoInicial)
+        public CompteBancari(string iban, string titular, double saldoInicial, bool bloquejat)
         {
+            this.iban = iban;
             this.titular = titular;
             saldo = saldoInicial;
-            bloquejat = false;
+            this.bloquejat = bloquejat;
         }
+
+        public CompteBancari(string iban, string titular, double saldoInicial)
+            : this(iban, titular, saldoInicial, false) { }
+
+        public CompteBancari(string iban, string titular)
+            : this(iban, titular, 0, false) { }
 
         // PROPIETATS
         public static double TipusInteresAnual
         {
-            get { return tipusInteresAnual; }
+            get { return CompteBancari.tipusInteresAnual; }
+            //get { return tipusInteresAnual; }
         }
 
-        public static void Titular
+        public string Iban
+        {
+            get { return iban; }
+        }
+
+        public string Titular
         {
             get { return titular; }
             private set { titular = value; }
         }
 
-        public static double Saldo
+        public double Saldo
         {
-            private get { return Saldo; }
+            get { return this.saldo; }
         }
 
         // MÈTODES D'INSTÀNCIA
+
+        public override bool Equals(object? obj)
+        {
+            bool iguals;
+            if (obj == null)
+                iguals = false;
+            else if (obj is not CompteBancari)
+                iguals = false;
+            else
+            {
+                CompteBancari other = (CompteBancari)obj;
+                iguals = this.iban == other.iban && this.titular == other.titular;  
+            }
+
+            return iguals;
+        }
+
+        public override string ToString()
+        {
+            string str = $"TITULAR:{titular} SALDO:{Saldo}";
+
+            if (bloquejat) str += " (BLOQUEJAT)";
+            else str += " (ACTIU)";
+
+            return str;    
+        }
+
         public void Ingressar(double quantitat)
         {
+            if (bloquejat)
+                throw new Exception("Compte bloquejat");
+            if (quantitat < 0)
+                throw new Exception("La quantitat no pot ser negativa");
+
             saldo += quantitat;
         }
 
         public bool Retirar(double quantitat)
         {
-            if (CompteBancari.TeSaldoSuficient(quantitat))
+            if (bloquejat)
+                throw new Exception("Compte bloquejat");
+            if (!this.TeSaldoSuficient(quantitat))
                 throw new Exception("Operació no permesa: Insuficient saldo.");
 
             saldo -= quantitat;
@@ -58,14 +106,24 @@ namespace DemoErrorsOO
         }
 
         // MÈTODES ESTATICS
-        private static bool Transaccio(CompteBancari origen, CompteBancari desti, double quantitat)
+        public static void Transaccio(
+            CompteBancari origen, 
+            CompteBancari desti, 
+            double quantitat)
         {
-            if (CompteBancari.TeSaldoSuficient(quantitat))
+
+            if (origen.bloquejat)
+                throw new Exception("Compte origen bloquejat");
+
+            if (!origen.TeSaldoSuficient(quantitat))
                 throw new Exception("Operació no permesa: Insuficient saldo.");
+
+            if (desti.bloquejat)
+                throw new Exception("Compte destí bloquejat");
 
             origen.saldo -= quantitat;
             desti.saldo += quantitat;
-            return true;
+
         }
     }
 }
--- OrientacioObjectes/CompteBancari/CompteBancari/Program.cs
+++ OrientacioObjectes/CompteBancari/CompteBancari/Program.cs
@@ -1,28 +1,77 @@
-﻿namespace CompteBancari
+﻿namespace DemoCompteBancari
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            CompteBancari c1 = new CompteBancari("Anna", 1000);
-            CompteBancari c2 = new CompteBancari("Marc", 500);
+            /*
+             * Demo inicial
+             */
+            CompteBancari cb1 = new CompteBancari("C100-83492483", "Anna", 1000);
+            CompteBancari cb2 = new CompteBancari("C100-83492483", "Anna", 100);
+            CompteBancari cb3 = cb1;
+
+
+            //COMPARACIONS AMB ==
+            if (cb1 == cb1)
+                Console.WriteLine("cb1 és == a cb1");
+            else
+                Console.WriteLine("cb1 és != a cb1");
+
+            if (cb1 == cb2)
+                Console.WriteLine("cb1 és == a cb2");
+            else
+                Console.WriteLine("cb1 és != a cb2");
+
+            if (cb1 == cb3)
+                Console.WriteLine("cb1 és == a cb3");
+            else
+                Console.WriteLine("cb1 és != a cb3");
+
+            //COMPARACIONS AMB Equals
+            if (cb1.Equals(cb1))
+                Console.WriteLine("cb1 és Equals a cb1");
+            else
+                Console.WriteLine("cb1 no és Equals a cb1");
+
+            if (cb1.Equals(cb2))
+                Console.WriteLine("cb1 és Equals a cb2");
+            else
+                Console.WriteLine("cb1 no és Equals a cb2");
+
+            if (cb1.Equals(cb3))
+                Console.WriteLine("cb1 és Equals a cb3");
+            else
+                Console.WriteLine("cb1 no és Equals a cb3");
+
+
+            /*
+             * Demo inicial
+             */
+            CompteBancari c1 = new CompteBancari("C100-83492483", "Anna", 1000);
+            CompteBancari c2 = new CompteBancari("C101-83492909","Marc", 500);
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
 
             c1.Ingressar(200);
             c2.Retirar(100);
-            Console.WriteLine(c1.Saldo());
-            Console.WriteLine(c2.Saldo());
+            Console.WriteLine(c1.Saldo);
+            Console.WriteLine(c2.Saldo);
 
-            c1.Transaccio(c1, c2, 150);
+            CompteBancari.Transaccio(c1, c2, 150);
 
-            Console.WriteLine(c1.GetSaldo());
-            Console.WriteLine(c2.GetSaldo());
+            Console.WriteLine(c1.Saldo);
+            Console.WriteLine(c2.Saldo);
 
-            Console.WriteLine(c1.saldo);
-            c2.Transaccio(c1, c2, 50);
-            CompteBancari.Transaccio(1000, c2, 50);
-            CompteBancari.Transaccio(c1, c2, "100");
-            Console.WriteLine(CompteBancari.origen);
-            c3.Ingressar(50);
+            CompteBancari.Transaccio(c1, c2, 50);
+            CompteBancari.Transaccio(c2, c1, 50);
+            CompteBancari.Transaccio(c1, c2, 100);
+            Console.WriteLine("Tipus interès anual: " + CompteBancari.TipusInteresAnual);
+            //c3.Ingressar(50); //no existeix l'objecte c3
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
         }
     }
 }
