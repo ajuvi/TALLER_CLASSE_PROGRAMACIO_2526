5bac8100ed4098843ceb0402e231eb1aedc428f3
ajuvi <102821845+ajuvi@users.noreply.github.com>
2026-01-29 08:20:31 +0100

    CompteBancari

---
+++ OrientacioObjectes/CompteBancari/CompteBancari/CompteBancari.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoErrorsOO
+{
+    internal class CompteBancari
+    {
+        // CAMPS
+        public string titular;
+        private double saldo;
+        private bool bloquejat;
+        private static double tipusInteresAnual = 0.03;
+
+        // CONSTRUCTORS
+        public CompteBancari(string titular, double saldoInicial)
+        {
+            this.titular = titular;
+            saldo = saldoInicial;
+            bloquejat = false;
+        }
+
+        // PROPIETATS
+        public static double TipusInteresAnual
+        {
+            get { return tipusInteresAnual; }
+        }
+
+        public static void Titular
+        {
+            get { return titular; }
+            private set { titular = value; }
+        }
+
+        public static double Saldo
+        {
+            private get { return Saldo; }
+        }
+
+        // MÈTODES D'INSTÀNCIA
+        public void Ingressar(double quantitat)
+        {
+            saldo += quantitat;
+        }
+
+        public bool Retirar(double quantitat)
+        {
+            if (CompteBancari.TeSaldoSuficient(quantitat))
+                throw new Exception("Operació no permesa: Insuficient saldo.");
+
+            saldo -= quantitat;
+            return true;
+        }
+
+        private bool TeSaldoSuficient(double quantitat)
+        {
+            return saldo >= quantitat;
+        }
+
+        // MÈTODES ESTATICS
+        private static bool Transaccio(CompteBancari origen, CompteBancari desti, double quantitat)
+        {
+            if (CompteBancari.TeSaldoSuficient(quantitat))
+                throw new Exception("Operació no permesa: Insuficient saldo.");
+
+            origen.saldo -= quantitat;
+            desti.saldo += quantitat;
+            return true;
+        }
+    }
+}
---
+++ OrientacioObjectes/CompteBancari/CompteBancari/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CompteBancari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CompteBancari c1 = new CompteBancari("Anna", 1000);
+            CompteBancari c2 = new CompteBancari("Marc", 500);
+
+            c1.Ingressar(200);
+            c2.Retirar(100);
+            Console.WriteLine(c1.Saldo());
+            Console.WriteLine(c2.Saldo());
+
+            c1.Transaccio(c1, c2, 150);
+
+            Console.WriteLine(c1.GetSaldo());
+            Console.WriteLine(c2.GetSaldo());
+
+            Console.WriteLine(c1.saldo);
+            c2.Transaccio(c1, c2, 50);
+            CompteBancari.Transaccio(1000, c2, 50);
+            CompteBancari.Transaccio(c1, c2, "100");
+            Console.WriteLine(CompteBancari.origen);
+            c3.Ingressar(50);
+        }
+    }
+}
